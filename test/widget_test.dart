397b1409bc521244f669250756946dd08b6e86a7
musickappa <musickappa1802@gmail.com>
2021-03-17 16:36:18 +0900

    firebaseの使い方 １．パッケージ名androidmanifest.xmlに書いてある ２．FirebaseのSDK追加 └app/build.gradle └build.gradle 3.AndroidX Migrationでエラーを解決かも？（私はこれはなかった） 4.app/build.gradleにmultiDexEnable trueを追加 5.app/build.gradleのminSdkVersionを変更（おそらく16から21？に） 6.app/build.gradleにimplementationを新たに追加 456については「64を超えるメソッドを使用するアプリ向けにmultidexを有効化する」というサイトをチェック

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,7 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_app_20210310/presentation/book_list/main/main.dart';
+import 'package:flutter_app_20210310/presentation/main/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 // import 'file:///C:/Users/music/AndroidStudioProjects/flutter_app_20210310/lib/main/main.dart';
